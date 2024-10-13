d5b7163a6b28eefc936221b7584627bddaa7a9a0
Hoangvu2825 <phoangvu2825@gmail.com>
2024-10-13 12:59:56 +0700

    thêm login

---
+++ Pha Lê/js_jquery/login.js
@@ -0,0 +1,18 @@
+$(function(){
+
+    $("#lop_phu_login").hide(); // ẩn form 
+
+    
+    $("#btnlogin").click(function() {
+        $("#lop_phu_login").fadeIn(); // Hiển thị form 
+    });
+
+    // Khi người dùng bấm vào lớp phủ bên ngoài để đóng
+    $("#lop_phu_login").click(function(event) {
+        if (event.target.id ) { //id của #lop_phu_login
+            $(this).fadeOut(); // Ẩn lớp phủ và form đăng nhập
+        }
+    });
+    
+});
+
