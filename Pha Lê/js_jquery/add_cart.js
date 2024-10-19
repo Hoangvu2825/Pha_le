d2644ac11d4eb4d0cb603731b0819f86c22cd13d
Hoangvu2825 <phoangvu2825@gmail.com>
2024-10-19 16:35:15 +0700

    19/10

---
+++ Pha Lê/js_jquery/add_cart.js
@@ -0,0 +1,24 @@
+$(function(){
+    $("#lop_phu_sp").hide(); // ẩn form 
+
+    
+    $("#btn-them").click(function() {
+        $("#lop_phu_sp").fadeToggle(); // Hiển thị form 
+        $('body').addClass('lock-scroll');  // Thêm class để khóa cuộn
+    });
+
+    // Khi người dùng bấm vào lớp phủ bên ngoài để đóng
+    $("#lop_phu_sp , #close-sp").click(function(event) {
+        if (event.target.id === 'lop_phu_sp' || event.target.id === 'close-sp') { //id của #lop_phu_login
+            $(this).fadeOut(); // Ẩn lớp phủ và form đăng nhập
+            $('body').removeClass('lock-scroll');  // Thêm class để khóa cuộn
+        }
+    });
+    
+    $("#close-sp").click(function(){
+        $("#lop_phu_sp").fadeOut(); // ẩn form   
+        $('body').removeClass('lock-scroll');  
+    })
+
+});
+
