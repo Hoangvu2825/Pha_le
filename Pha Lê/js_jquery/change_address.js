247f02a56c459034f705788e2f1729dee1388564
Hoangvu2825 <phoangvu2825@gmail.com>
2024-10-19 10:12:27 +0700

    19/10

--- Pha Lê/js_jquery/change_address.js
+++ Pha Lê/js_jquery/change_address.js
@@ -1,24 +1,19 @@
 $(function(){
-    $("#lop_phu_login").hide(); // ẩn form 
+    $("#lop_phu_address").hide(); // ẩn form 
 
     
-    $("#btnlogin").click(function() {
-        $("#lop_phu_login").fadeToggle(); // Hiển thị form 
+    $("#btn_doi-phuong-thuc").click(function() {
+        $("#lop_phu_address").fadeToggle(); // Hiển thị form 
         $('body').addClass('lock-scroll');  // Thêm class để khóa cuộn
     });
 
     // Khi người dùng bấm vào lớp phủ bên ngoài để đóng
-    $("#lop_phu_login, #btnlogin").click(function(event) {
+    $("#lop_phu_address").click(function(event) {
         if (event.target.id ) { //id của #lop_phu_login
             $(this).fadeOut(); // Ẩn lớp phủ và form đăng nhập
             $('body').removeClass('lock-scroll');  // Thêm class để khóa cuộn
         }
     });
     
-    $(".nut_login").click(function(){
-        $("#lop_phu_login").fadeOut(); // ẩn form   
-        $('body').removeClass('lock-scroll');  
-    })
-
-});
+})
 
